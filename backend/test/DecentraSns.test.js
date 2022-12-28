685818abf79c1fd2f389870f63a4f1bca89ad0f8
sheegull <89774989+sheegulls@users.noreply.github.com>
2022-12-29 02:39:47 +0900

    🧪 test: Should return error
    cannot pass an empty text

--- backend/test/DecentraSns.test.js
+++ backend/test/DecentraSns.test.js
@@ -1,10 +1,10 @@
 const { ethers } = require("hardhat");
+const { assert } = require("console");
 const { expect } = require("chai");
 const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
 
 describe("DecentraSns", function () {
-    // let text = "SampleText";
-    // 各テストで使用されるヘルパー関数
+    // ヘルパー関数
     async function deployContract() {
         const [owner, addr1, addr2] = await ethers.getSigners();
 
@@ -17,24 +17,33 @@ describe("DecentraSns", function () {
 
     // 新規投稿機能の確認
     describe("uploadPost", function () {
-        it("Should track posts uploaded by users", async function () {
-            const { addr1, decentrasns } = await loadFixture(deployContract);
-            // ユーザーが新規投稿
-            // await expect(decentrasns.connect(addr1).uploadPost(text))
-            //     .to.emit(decentrasns, "PostCreated")
-            //     .withArgs(0, addr1.address, text, await decentrasns.send("evm_mine"), 0, false);
-            // const postIds = await decentrasns._postIds();
-            // expect(postIds).to.equal(1);
-            const postTxn = await decentrasns.connect(addr1).uploadPost("addr1 post#1");
-            await postTxn.wait();
-            const postTxn2 = await decentrasns.connect(addr1).uploadPost("addr1 post#2");
-            await postTxn2.wait();
-            const postTxn3 = await decentrasns.connect(addr1).uploadPost("addr1 post#3");
-            await postTxn3.wait();
-            const postTxn4 = await decentrasns.uploadPost("addr2 post#1");
-            await postTxn4.wait();
-            const postTxn5 = await decentrasns.uploadPost("addr2 post#2");
-            await postTxn5.wait();
+        it("Should return error", async function () {
+            const { owner, decentrasns } = await loadFixture(deployContract);
+            await expect(decentrasns.connect(owner).uploadPost("")).to.be.revertedWith(
+                "Cannot pass an empty text"
+            );
         });
     });
+
+    // describe("uploadPost", function () {
+    //     it("Should track posts uploaded by users", async function () {
+    //         const { addr1, decentrasns } = await loadFixture(deployContract);
+    //         // ユーザーが新規投稿
+    //         // await expect(decentrasns.connect(addr1).uploadPost(text))
+    //         //     .to.emit(decentrasns, "PostCreated")
+    //         //     .withArgs(0, addr1.address, text, await decentrasns.send("evm_mine"), 0, false);
+    //         // const postIds = await decentrasns._postIds();
+    //         // expect(postIds).to.equal(1);
+    //         const postTxn = await decentrasns.connect(addr1).uploadPost("addr1 post#1");
+    //         await postTxn.wait();
+    //         const postTxn2 = await decentrasns.connect(addr1).uploadPost("addr1 post#2");
+    //         await postTxn2.wait();
+    //         const postTxn3 = await decentrasns.connect(addr1).uploadPost("addr1 post#3");
+    //         await postTxn3.wait();
+    //         const postTxn4 = await decentrasns.uploadPost("addr2 post#1");
+    //         await postTxn4.wait();
+    //         const postTxn5 = await decentrasns.uploadPost("addr2 post#2");
+    //         await postTxn5.wait();
+    //     });
+    // });
 });
