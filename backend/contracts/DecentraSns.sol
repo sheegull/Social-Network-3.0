9210f1eddbd46b107441937937890dc026ac2300
sheegull <89774989+sheegulls@users.noreply.github.com>
2022-12-29 02:32:59 +0900

    🐛 struct Post, functions

--- backend/contracts/DecentraSns.sol
+++ backend/contracts/DecentraSns.sol
@@ -28,14 +28,14 @@ contract DecentraSns {
     Post[] public allPosts;
 
     mapping(address => User) public users;
-    mapping(address => Post[]) public likedList;
+    mapping(address => uint256[]) public likedList;
 
     // 新しくpostが作成されたときに呼び出されるイベント
     event newPosted(address indexed from);
 
     // 新しくpostがいいねされたときに呼び出されるイベント
     event newLiked(
-        uint256 postId,
+        uint256 id,
         address indexed from,
         string text,
         uint256 timestamp,
@@ -52,15 +52,15 @@ contract DecentraSns {
         uint256 newPostId = _postIds.current();
 
         Post memory newPost;
-        Post.id = newPostId + 1;
-        Post.from = msg.sender;
-        Post.text = _text;
-        Post.timestamp = block.timestamp;
-        Post.like = 0;
-        Post.isLiking = false;
+        newPost.id = newPostId + 1;
+        newPost.from = msg.sender;
+        newPost.text = _text;
+        newPost.timestamp = block.timestamp;
+        newPost.like = 0;
+        newPost.isLiking = false;
 
         // 文字数制限（0文字投稿禁止）
-        require(bytes(_text).length > 0, "Cannnot pass an empty text");
+        require(bytes(_text).length > 0, "Cannot pass an empty text");
 
         allPosts.push(newPost);
         _postIds.increment();
@@ -73,7 +73,7 @@ contract DecentraSns {
     function addLike(uint256 _id) public {
         // 二重いいねを禁止
         for (uint256 i = 0; i < likedList[msg.sender].length; i++) {
-            if (likedList[msg.sender][i].id == _id) return;
+            if (likedList[msg.sender][i] == _id) return;
         }
 
         // いいね数の更新
@@ -86,7 +86,7 @@ contract DecentraSns {
 
         // postにいいねが増えたのでイベント発火
         emit newLiked(
-            resPosts[_id].postId,
+            resPosts[_id].id,
             resPosts[_id].from,
             resPosts[_id].text,
             resPosts[_id].timestamp,
@@ -107,7 +107,7 @@ contract DecentraSns {
 
         // postのいいねが減ったのでイベント発火
         emit newLiked(
-            resPosts[_id].postId,
+            resPosts[_id].id,
             resPosts[_id].from,
             resPosts[_id].text,
             resPosts[_id].timestamp,
