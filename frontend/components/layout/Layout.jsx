d4bea80fc465ad085742aa9900a8eda36ca4065e
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-01-03 07:50:27 +0900

    💄 update favicon design

--- frontend/components/layout/Layout.jsx
+++ frontend/components/layout/Layout.jsx
@@ -6,7 +6,10 @@ export default function Layout({ children, home }) {
     return (
         <div className={styles.container}>
             <Head>
-                <link rel="icon" href="/favicon.png" />
+                <link
+                    rel="icon"
+                    href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🧜</text></svg>"
+                />
                 <meta
                     name="description"
                     content="It is a message dapp that exchanges text and AVAX"
