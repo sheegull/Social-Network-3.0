f951cbda4ce89cc8e6ca4fdcf5a9609839dddf91
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-01-05 11:12:45 +0900

    💄 check Feed UI

--- frontend/components/Feed/Feed.jsx
+++ frontend/components/Feed/Feed.jsx
@@ -24,66 +24,52 @@ const Feed = () => {
         <div className="content-container">
             <TopNav />
             <div className="content-list">
-                {/* <Post
-                    name="Ada"
-                    timestamp="one week ago"
-                    text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit
-          amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem
-          ipsum dolor sit amet consectetur adipisicing elit.`}
-                />
-                <Post name="Leon" timestamp="one week ago" text={`Lorem ipsum dolor. `} />
-                <Post name="Jill" timestamp="5 days ago" text={`Lorem.`} />
-                <Post
-                    name="Ellie"
-                    timestamp="4 days ago"
-                    text={`Lorem ipsum dolor sit amet consectetur adipisicing elit. `}
-                /> */}
                 {currentAccount &&
                     allPosts
                         .slice(0)
                         .reverse()
                         .map((post, index) => {
                             return (
-                                <div
+                                <Post
                                     key={index}
-                                    style={{
-                                        width: "600px",
-                                        backgroundColor: "gray",
-                                        marginTop: "16px",
-                                        padding: "8px",
-                                    }}
-                                >
-                                    <div>#{post.id}</div>
-                                    <div>@{post.from}</div>
-                                    <div>
-                                        Posted at:
-                                        {new Date(post.timestamp.toNumber().toString() * 1000)
-                                            .toString()
-                                            .substring(
-                                                0,
-                                                new Date(
-                                                    post.timestamp.toNumber().toString() * 1000
-                                                )
-                                                    .toString()
-                                                    .indexOf("GMT")
-                                            )}
-                                    </div>
-                                    <div className="text">Text:{post.text}</div>
-                                    <div>
-                                        {currentAccount && (
-                                            <button
-                                                aria-label="favorite"
-                                                size="small"
-                                                color="primary"
-                                                onClick={() => changeLikePost(post.id)}
-                                            >
-                                                <HiOutlineHeart />
-                                                {post.likeCount}
-                                            </button>
-                                        )}
-                                    </div>
-                                </div>
+                                    address={post.from}
+                                    // timestamp={post.timestamp}
+                                    text={post.text}
+                                    // <div
+                                    //     key={index}
+
+                                    // >
+                                    //     <div>#{post.id}</div>
+                                    //     <div>@{post.from}</div>
+                                    //     <div>
+                                    //         Posted at:
+                                    //         {new Date(post.timestamp.toNumber().toString() * 1000)
+                                    //             .toString()
+                                    //             .substring(
+                                    //                 0,
+                                    //                 new Date(
+                                    //                     post.timestamp.toNumber().toString() * 1000
+                                    //                 )
+                                    //                     .toString()
+                                    //                     .indexOf("GMT")
+                                    //             )}
+                                    //     </div>
+                                    //     <div className="text">Text:{post.text}</div>
+                                    //     <div>
+                                    //         {currentAccount && (
+                                    //             <button
+                                    //                 aria-label="favorite"
+                                    //                 size="small"
+                                    //                 color="primary"
+                                    //                 onClick={() => changeLikePost(post.id)}
+                                    //             >
+                                    //                 <HiOutlineHeart />
+                                    //                 {post.likeCount}
+                                    //             </button>
+                                    //         )}
+                                    //     </div>
+                                    // </div>
+                                />
                             );
                         })}
             </div>
@@ -96,7 +82,7 @@ const Feed = () => {
     );
 };
 
-const Post = ({ name, timestamp, text }) => {
+const Post = ({ address, timestamp, text }) => {
     const seed = Math.round(Math.random() * 100);
     return (
         <div className={"post"}>
@@ -110,7 +96,7 @@ const Post = ({ name, timestamp, text }) => {
 
             <div className="post-content">
                 <p className="post-owner">
-                    {name}
+                    {address}
                     <small className="timestamp">{timestamp}</small>
                 </p>
                 <p className="post-text">{text}</p>
