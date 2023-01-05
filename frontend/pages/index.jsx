0c6632a0e69999e5ce661f491268c7bd711bdea8
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-01-06 07:26:17 +0900

    🎨 delete RequireWallet files

--- frontend/pages/index.jsx
+++ frontend/pages/index.jsx
@@ -2,7 +2,6 @@ import styles from "../styles/Home.module.css";
 import { useState } from "react";
 import Link from "next/link";
 import Layout from "../components/layout/Layout";
-import RequireWallet from "../components/layout/RequireWallet";
 import { useWallet } from "../hooks/useWallet";
 import { useDecentrasnsContract } from "../hooks/useDecentrasnsContract";
 import PostBox from "../components/Form/PostBox";
@@ -10,6 +9,9 @@ import { HiOutlineHeart } from "react-icons/hi2";
 import SideBar from "../components/Sidebar/Sidebar";
 import ChannelBar from "../components/ChannelBar/ChannelBar";
 import Feed from "../components/Feed/Feed";
+import Image from "next/image";
+import metamaskLogo from "../public/metamask.png";
+import TopNav from "../components/TopNav/TopNav";
 
 const style = {
     wrapper: `flex h-screen w-screen overflow-hidden select-none bg-[#121212] text-[#f2f2f2] font-light`,
@@ -18,7 +20,7 @@ const style = {
     feed: `w-1/2 mr-10 `,
     widgets: "w-1/4 mr-10 my-7 h-screen w-screen overflow-y-scroll scrollbar-hide",
     loginContainer: `w-full h-full flex flex-col justify-center items-center pb-48`,
-    walletConnectButton: `text-2xl text-black bg-white font-bold mb-[-3rem] mt-[3rem] px-6 py-4 rounded-full cursor-pointer hover:bg-[#d7dbdc]`,
+    connectWalletButton: `text-2xl text-black bg-white font-bold mb-[-3rem] mt-[3rem] px-6 py-4 rounded-full cursor-pointer hover:bg-[#d7dbdc]`,
     loginContent: `text-3xl font-bold text-center mt-24`,
 };
 
@@ -38,28 +40,50 @@ const Home = () => {
 
     return (
         <Layout home>
-            <RequireWallet currentAccount={currentAccount} connectWallet={connectWallet}>
+            {currentAccount ? (
                 <div className="flex">
                     <SideBar />
                     <ChannelBar />
                     <Feed />
                 </div>
-                <div className="mainContainer">
-                    <div className="dataContainer">
-                        {/* {isLoading ? (
-                            <div>Loading...</div>
-                        ) : (
-                            <PostBox
-                                uploadPost={(text) => {
-                                    uploadPost({ text });
-                                }}
-                            />
-                        )} */}
-                    </div>
+            ) : (
+                <div className="flex">
+                    <SideBar />
+                    <ChannelBar />
+                    <Login connectWallet={connectWallet} />
                 </div>
-            </RequireWallet>
+            )}
         </Layout>
     );
 };
 
 export default Home;
+
+const Login = ({ connectWallet }) => {
+    return (
+        <div className={style.loginContainer}>
+            <Image alt="metamask" src={metamaskLogo} width={200} height={200} priority={true} />
+            <div className={style.connectWalletButton} onClick={() => connectWallet()}>
+                Connect Wallet
+            </div>
+            <div className={style.loginContent}>
+                <p>Welcome to DecentraSNS 🧜</p>
+                <p>Connect to Metamask !!</p>
+            </div>
+        </div>
+    );
+};
+
+/* <div className="mainContainer">
+    <div className="dataContainer">
+        {isLoading ? (
+            <div>Loading...</div>
+        ) : (
+            <PostBox
+                uploadPost={(text) => {
+                    uploadPost({ text });
+                }}
+            />
+        )}
+    </div>
+</div>; */
