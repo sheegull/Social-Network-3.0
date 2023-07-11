aaf9614368a8df75d1149a2f352629e1c1902641
sheegull <89774989+sheegulls@users.noreply.github.com>
2023-07-12 00:19:32 +0900

    🎨 change variable name

--- backend/scripts/deploy.js
+++ backend/scripts/deploy.js
@@ -1,22 +1,22 @@
 const { ethers } = require("hardhat");
 
 async function main() {
-    const Decentrasns = await ethers.getContractFactory("decentrasns");
-    const decentrasns = await Decentrasns.deploy();
-    await decentrasns.deployed();
+  const Dsns = await ethers.getContractFactory("dsns");
+  const dsns = await Dsns.deploy();
+  await dsns.deployed();
 
-    console.log("deploy success 🎉🎉");
-    console.log("Contract deployed to: ", decentrasns.address);
+  console.log("deploy success 🎉🎉");
+  console.log("Contract deployed to: ", dsns.address);
 }
 
 const runMain = async () => {
-    try {
-        await main();
-        process.exit(0);
-    } catch (error) {
-        console.log(error);
-        process.exit(1);
-    }
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
 
 runMain();
